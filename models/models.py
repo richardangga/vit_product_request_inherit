56b3223cc0152d1446ccc0682e3242da8b3bfecc
richardangga <richard.angga51@gmail.com>
2020-01-15 02:14:56 -0500

    upgrade inherit product request

--- models/models.py
+++ models/models.py
@@ -2,19 +2,28 @@
 
 from odoo import models, fields, api
 from odoo import tools
-import time
-import logging
 from odoo.tools.translate import _
-from collections import defaultdict
-from odoo.exception import UserError
 
 class ProductRequestInherit(models.Model):
     _name = "product.request.inherit"
-    _inherit = ['vit.product.request.line','vit.product.request']
+    _inherit = 'vit.product.request.line'
 
-    product_qty = fields.Many2one(comodel_name='',string="Quantity")
+    # product_id = fields.Many2one(comodel_name='product.product', string='Product')
 
-    @api.multi
+    # @api.multi
+     def create_pr(self, product_id, request_line=None):
+        cr=self.env.cr
+        purchase_requisition  = self.env['purchase.requisition']
+        product_request       = self.env['vit.product.request']
+        product_req = self.env['vit.product.request.line']
+        origins = product_request.browse(request_line['origins'])
 
+        line_department_ids = []
+        for x in request_line['qty_per_dept'].keys():
+            line_department_ids.append( (0,0,{
+                'qty':1,
+                'request_id':request_line['qty_per_dept'][x]['request_id'],
+                'warehouse_id':request_line['qty_per_dept'][x]['warehouse_id'],
+                'department_id': self.department_id.id}) )
 
 #
